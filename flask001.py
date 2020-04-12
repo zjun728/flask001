f191a84b80cccf8fb797b60bb84452a8c4b5fe17
zjun <503280155@qq.com>
2020-04-12 23:29:37 +0800

    202004122329

--- flask001.py
+++ flask001.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request
+from flask import Flask, url_for, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -8,8 +8,19 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/login/', methods=['GET', 'POST'])
+def user_login():
+    return render_template("user_login.html")
+
+
 @app.route('/regist/', methods=['GET', 'POST'])
 def user_regist():
+    if request.method == "POST":
+        print(request.form)
+        print(request.form["user_name"])
+        print(request.form["user_pwd"])
+        print(request.form)
+        return redirect(url_for("user_login"))  # 重定向页面 生成url 执行 user_login 函数 跳转到登录界面
     return render_template("user_regist.html")
 
 
