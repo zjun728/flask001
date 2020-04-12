d05fdc3e1b2cb5460ea25d02a5d0624eed1c6443
zjun <503280155@qq.com>
2020-04-12 13:36:12 +0800

    04121336

--- flask001.py
+++ flask001.py
@@ -1,25 +1,17 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/<name>')
-def hello_world(name=None):
-    # return 'Hello World!'
-    print(name)
-    # return render_template("index.html", name=name)
-    user=['qq','q1','q2','q3','q4']
-    return render_template("index.html", name=name,user=user)
+def index():
+    return render_template("index.html")
 
 
-@app.route('/post/<username>/<int:post_id>')
-def show_post(username, post_id):
-    return 'user %s post %d' % (username, post_id)
+@app.route('/regist/', methods=['GET', 'POST'])
+def user_regist():
+    return render_template("user_regist.html")
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    # with app.test_request_context():
-    #     print(url_for('hello_world'))
-    #     print(url_for('show_post', username='JohnDoe',post_id=123))
