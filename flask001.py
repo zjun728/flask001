730643d290eb2233a58c31763c407d3383f1ce24
zjun <503280155@qq.com>
2020-04-13 23:13:01 +0800

    202004132312

--- flask001.py
+++ flask001.py
@@ -1,6 +1,27 @@
 from flask import Flask, url_for, render_template, request, redirect
 
+from model import User
+
+import sqlite3
+
 app = Flask(__name__)
+app.config["DATABASE"]="database.db"
+
+
+def connect_db():
+    """Connects to the specific database."""
+    db = sqlite3.connect(app.config['DATABASE'])
+    return db
+
+#初始化数据库
+def init_db():
+    with app.app_context():
+        db = connect_db()
+        with app.open_resource('schema.sql', mode='r') as f:   # mode='r'只读模式 rb可读，可写
+            db.cursor().executescript(f.read())    #执行sql脚本
+        db.commit()   #提交sql表
+
+
 
 
 @app.route('/')
@@ -17,10 +38,18 @@ def user_login():
 def user_regist():
     if request.method == "POST":
         print(request.form)
-        print(request.form["user_name"])
-        print(request.form["user_pwd"])
-        print(request.form)
-        return redirect(url_for("user_login"))  # 重定向页面 生成url 执行 user_login 函数 跳转到登录界面
+        user = User()
+        user.name = request.form["user_name"]
+        user.pwd = request.form["user_pwd"]
+        user.age = request.form["user_age"]
+        user.birthday = request.form["user_birthday"]
+        user.email = request.form["user_email"]
+        user.face = request.form["user_face"]
+        print(user.name)
+
+        # username作为查询参数带到url中去
+        ## 重定向页面 生成url 执行 user_login 函数 跳转到登录界面
+        return redirect(url_for("user_login", username=user.name))
     return render_template("user_regist.html")
 
 
---
+++ model.py
@@ -0,0 +1,11 @@
+# coding utf8
+
+
+class User:
+    def __init__(self, name=None, pwd=None, email=None, age=None, birthday=None, face=None):
+        self.name = name
+        self.pwd = pwd
+        self.email = email
+        self.age = age
+        self.birthday = birthday
+        self.face = face
