66fe4c5aef4f519ebdf752a11aa6996e4eab269b
zjun <503280155@qq.com>
2020-04-14 23:04:22 +0800

    202004142304

--- flask001.py
+++ flask001.py
@@ -36,13 +36,30 @@ def teardown_request(exception):
         g.db.close()
 
 
-def instert_user_to_db(users):
-    sql_instert = "INSERT INTO users (name, pwd,email,age,birthday,face) values (?, ?, ?, ?, ?, ?)"  # 插入一条语句到users表中
-    args = [users.name, users.pwd, users.email, users.age, users.birthday, users.face]
+# 往数据库插入数据
+def instert_user_to_db(user):
+    # sql_instert = "INSERT INTO users (name, pwd,email,age,birthday,face) VALUES (?, ?, ?, ?, ?, ?)"  # 插入一条语句到users表中
+
+    # 构造  (name, pwd,email,age,birthday,face)
+    user_attrs = user.getAttres()
+    # 构造  “values (?, ?, ?, ?, ?, ?)"
+    values = "VALUES("
+    last_attr = user_attrs[-1]
+    for attr in user_attrs:
+        if attr != last_attr:
+            values += "?,"
+        else:
+            values += "?"
+    values += ")"
+    sql_instert = "INSERT INTO users" + str(user_attrs) + values  # 插入一条语句到users表中
+
+    # args = [user.name, user.pwd, user.email, user.age, user.birthday, user.face]
+    args = user.tolist()
     g.db.execute(sql_instert, args)
     g.db.commit()
 
 
+# 查询数据库所有数据
 def query_users_from_db():
     users = []
     sql_select = "SELECT *FROM users"
@@ -51,19 +68,53 @@ def query_users_from_db():
     for item in cur.fetchall():
         user = User()
         # item[0] 为id
-        user.name = item[1]
-        user.pwd = item[2]
-        user.email = item[3]
-        user.age = item[4]
-        user.birthday = item[5]
-        user.face = item[6]
+        # user.name = item[1]
+        # user.pwd = item[2]
+        # user.email = item[3]
+        # user.age = item[4]
+        # user.birthday = item[5]
+        # user.face = item[6]
+
+        user.fromList(item[1:])  # 第一位为id 从第二位才开始赋值
+
         users.append(user)
     return users
     pass
 
 
+# 查询一条数据
+def query_user_by_name(user_name):
+    sql_select = "SELECT *FROM users WHERE name =?"
+    args = [user_name]
+    cur = g.db.execute(sql_select, args)
+    items = cur.fetchall()  # 取出第一条数据
+    if len(items) < 1:
+        return None
+    first_item = items[0]
+    user = User()
+    # item[0] 为id
+    # user.name = first_item[1]
+    # user.pwd = first_item[2]
+    # user.email = first_item[3]
+    # user.age = first_item[4]
+    # user.birthday = first_item[5]
+    # user.face = first_item[6]
+
+    user.fromList(first_item[1:])  # 第一位为id 从第二位才开始赋值
+    return user
+
+
+# 按照条件（name）删除一条数据
+def delete_user_by_name(user_name):
+    dellete_sql = "DELETE FROM users WHERE name=?"  # DELETE FROM users 删除全部数据
+    args = [user_name]
+    g.db.execute(dellete_sql, args)
+    g.db.commit()
+
+
 @app.route('/')
 def index():
+    delete_user_by_name("123")
     return render_template("index.html")
 
 
@@ -71,7 +122,11 @@ def index():
 def user_login():
     users = query_users_from_db()
     for user in users:
-        print(user.name, user.pwd, user.email, user.birthday, user.age, user.face)
+        print(user.tolist())
+    print("==========================")
+    user_one = query_user_by_name("123")
+    if user_one:
+        print(user_one.tolist())
     return render_template("user_login.html")
 
 
--- model.py
+++ model.py
@@ -9,3 +9,17 @@ class User:
         self.age = age
         self.birthday = birthday
         self.face = face
+
+    def tolist(self):
+        return [self.name, self.pwd, self.email, self.age, self.birthday, self.face]
+
+    def fromList(self, user_info):
+        self.name = user_info[0]
+        self.pwd = user_info[1]
+        self.email = user_info[2]
+        self.age = user_info[3]
+        self.birthday = user_info[4]
+        self.face = user_info[5]
+
+    def getAttres(self):
+        return ("name", "pwd", "email", "age", "birthday", "face")
